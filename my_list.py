91790c66aa073ba33cd90d667adf477343404663
kevDevops <144397049+kevDevops@users.noreply.github.com>
2024-03-14 07:31:36 +0300

    Update my_list.py

--- my_list.py
+++ my_list.py
@@ -1,17 +1,9 @@
-#empty  list called my_list
-my_list=""
-#appending elements to my list
-my_list.extend=[10,20,30,40]
-#inserting values
+my_list=[]
+my_list.extend=([10,20,30,40])
 my_list.insert=(1,15)
-#extend list with another list
 my_list.extend=([50,60,70])
-#removing end values from my list
 my_list.pop=()
-#sort my list
 my_list.sort=()
-#Find and print the index of the value 30 in my_list.
 index_30 = my_list index(30)
 print("Index of value 30:", index_30)
-# Print the updated list
 print("Updated my_list:", my_list)
